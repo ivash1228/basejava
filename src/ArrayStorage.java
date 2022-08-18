beb85aaeed3ebea0970a5d674f254c73236ab537
ivash1228 <ivash1228@gmail.com>
2022-08-18 09:06:02 -0400

    Added countResume field

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,15 +5,18 @@ import java.util.Arrays;
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int countResume = 0;
 
     void clear() {
         for (int i = 0; i < size(); i++) {
             storage[i] = null;
         }
+        countResume = 0;
     }
 
     void save(Resume r) {
         storage[size()] = r;
+        countResume++;
     }
 
     Resume get(String uuid) {
@@ -36,6 +39,7 @@ public class ArrayStorage {
         if (index >= 0) {
             System.arraycopy(storage, index + 1, storage, index, size - index);
             storage[size] = null;
+            countResume--;
         }
     }
 
@@ -47,11 +51,6 @@ public class ArrayStorage {
     }
 
     int size() {
-        int counter = 0;
-        for (Resume r : storage) {
-            if (r == null) break;
-            counter++;
-        }
-        return counter;
+        return countResume;
     }
 }
